b5dd2263af97d9ab1e254cdd17126aa62ff2f476
Zefuri <39730075+Zefuri@users.noreply.github.com>
2020-11-27 17:35:49 +0100

    Update Technicien.java

--- model/Technicien.java
+++ model/Technicien.java
@@ -18,16 +18,4 @@ public class Technicien {
 		System.out.println(mdp + "test"+ this.mdp);
 		return this.mdp.equals(mdp);
 	}
-	
-	public void delDVD(DVD dvd) {
-		
-	}
-	
-	public void addDVD(DVD dvd) {
-		
-	}
-	
-	public void DVDIsBroken(DVD dvd) {
-		
-	}
 }
